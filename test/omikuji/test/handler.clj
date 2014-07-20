01d182dbe9b1b1bb5b7b77af444519657e7a75f8
syobochim <MarilynMonroe0226@gmail.com>
2014-07-20 18:16:13 +0900

    テストファイルが括弧の数間違ってたので修正

--- test/omikuji/test/handler.clj
+++ test/omikuji/test/handler.clj
@@ -6,7 +6,7 @@
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
+      (is (= (:status response) 200))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
