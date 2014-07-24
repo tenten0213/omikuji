a6c2df3b001a441fbbfda417c8510708a0e9748b
syobochim <MarilynMonroe0226@gmail.com>
2014-07-24 20:43:20 +0900

    shuffleをhomeに

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -3,7 +3,15 @@ angular.module('omikuji', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home/home.html', 
-        controller: 'HomeController'})
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeController',
+        resolve: {
+          resolvedEntry: [
+              'Entry', function (Entry) {
+                  return Entry.query();
+              }
+          ]
+        }
+      })
       .otherwise({redirectTo: '/'});
   }]);
--- resources/public/js/home/home-controller.js
+++ resources/public/js/home/home-controller.js
@@ -1,3 +1,11 @@
+'use strict';
+
 angular.module('omikuji')
-  .controller('HomeController', ['$scope', function ($scope) {
+  .controller('HomeController', ['$scope', 'resolvedEntry',
+        function ($scope, resolvedEntry) {
+            $scope.entries = resolvedEntry;
+
+            $scope.roulette = function () {
+                $scope.entries = _.shuffle($scope.entries)
+            };
   }]);
