7e174656f72ac9ee3708be159a2e39cb9e429c64
tenten0213 <takehito.0213@gmail.com>
2014-07-24 20:19:39 +0900

    move omikuji to home

--- resources/public/js/home/home-controller.js
+++ resources/public/js/home/home-controller.js
@@ -1,3 +1,12 @@
+'use strict';
+
 angular.module('omikuji')
-  .controller('HomeController', ['$scope', function ($scope) {
-  }]);
+.controller('HomeController', ['$scope', 'resolvedEntry',
+  function ($scope, resolvedEntry, Entry) {
+    $scope.entries = resolvedEntry;
+
+    $scope.roulette = function () {
+      $scope.entries = _.shuffle($scope.entries);
+    };
+  }
+]);
