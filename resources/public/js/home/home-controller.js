a02918696fc7c1d947fdba5cf45b81fad38c52bf
syobochim <MarilynMonroe0226@gmail.com>
2014-07-31 12:06:35 +0900

    うごかない…

--- resources/public/js/home/home-controller.js
+++ resources/public/js/home/home-controller.js
@@ -4,16 +4,21 @@ angular.module('omikuji')
     .controller('HomeController', ['$scope', 'resolvedEntry',
         function ($scope, resolvedEntry) {
             $scope.entries = resolvedEntry;
+            $scope.lines = resolvedEntry;
 
-            $scope.roulette = function (index) {
-                $scope.tableshow = true;
+            $scope.roulette = function () {
                 $scope.entries = _.shuffle($scope.entries);
-                angular.forEach($scope.entries, function (value, key) {
-                    setTimeout($scope.lines = value, 2000)
-                });
+                angular.forEach($scope.entries, function (entry, key) {
+                        $scope.lines.push(entry)
+                    }
+                );
             };
 
             $scope.nekohide = function () {
-                $scope.tableshow = false;
+                angular.forEach($scope.lines, function (value, key) {
+                    setTimeout(function () {
+                        $scope.lines.pop(value)
+                    }, 3);
+                });
             }
         }]);
